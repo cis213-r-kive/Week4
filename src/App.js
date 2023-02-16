5974498fc8bf05ce312adc7650475684ffbae405
hannah pelingon <hannahpelingon@hannahs-MacBook-Pro.local>
2023-02-16 16:50:27 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, this is Hannah
         </p>
         <a
           className="App-link"
